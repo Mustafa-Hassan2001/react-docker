355bdcf5c4f78601af8b1b91100558123112bfba
Mustafa-Hassan2001 <mustafahassanis2001@gmail.com>
2024-03-18 00:25:17 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+      </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:20-alpine
+
+RUN addgroup app && adduser -S -G app app
+
+USER app
+
+WORKDIR /app
+
+COPY package*.json ./
+
+USER root
+
+RUN chown -R app:app .
+
+USER app
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 5173
+
+CMD npm run dev
